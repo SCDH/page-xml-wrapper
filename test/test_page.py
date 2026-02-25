f3d34b9170d10c0e62f1048d2d2b5ea60b7e1b77
Katharina Dietz <kdietz@uni-muenster.de>
2026-02-25 14:58:53 +0100

    Add assertion for missing file - reformatting

--- test/test_page.py
+++ test/test_page.py
@@ -391,9 +391,7 @@ def test_from_xml_file_example(tmp_path: Path) -> None:
 
 def test_from_missing_xml_file(tmp_path: Path) -> None:
     missing_file = tmp_path / "does_not_exist.xml"
-
     assert not missing_file.exists()
-
     with pytest.raises(FileNotFoundError):
         Page.from_xml_file(missing_file)
 
